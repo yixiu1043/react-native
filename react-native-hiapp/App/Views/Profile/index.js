95084a9f339936aca54f436c0e5eb364b356b506
yueyang8389 <yueyang8389@gmail.com>
2019-06-27 16:57:16 +0800

    finish profile page

--- react-native-hiapp/App/Views/Profile/index.js
+++ react-native-hiapp/App/Views/Profile/index.js
@@ -1,70 +1,64 @@
-import React from 'react'
-import connect from 'redux-connect-decorator'
-import config from '@Config'
-import styles from '@Styles'
-import { View, StyleSheet } from 'react-native'
-import { ListItem } from 'react-native-elements'
-import t from '@Localize'
+import React from 'react';
+import connect from 'redux-connect-decorator';
+import config from '@Config';
+import styles from '@Styles';
+import { View, StyleSheet } from 'react-native';
+import { ListItem } from 'react-native-elements';
+
+const viewStyles = StyleSheet.create({
+  container: {
+    ...styles.container,
+    paddingTop: 20,
+  },
+});
 
 @connect(state => ({
-  user: state.app.user
+  user: state.app.user,
 }))
-
-export default class ProfileScreen extends React.Component {
-  static navigationOptions = _ => {
-    return {
-      ...config.defaultNavigation,
-      title: t('global.profile'),
-    }
-  }
+class ProfileScreen extends React.Component {
+  static navigationOptions = () => ({
+    ...config.defaultNavigation,
+    title: '个人信息',
+  })
 
   render() {
+    const { user } = this.props;
     return (
       <View style={viewStyles.container}>
         <View>
           <ListItem
             topDivider
             bottomDivider
-            title={t('settings.avatar')}
+            title="头像"
             rightAvatar={{
               size: 65,
               source: {
-                uri: this.props.user.avatar_url
-              }
+                uri: user.avatar,
+              },
             }}
           />
           <ListItem
             bottomDivider
-            title={t('settings.nickname')}
-            rightTitle={this.props.user.nick_name}
+            title="UID"
+            rightTitle={user.userId}
           />
         </View>
         <View style={{ marginTop: 20 }}>
           <ListItem
             topDivider
             bottomDivider
-            title={t('settings.gender')}
-            rightTitle={this.formatGender()}
+            title="性别"
+            rightTitle="未设置"
           />
           <ListItem
             bottomDivider
-            title={t('settings.location')}
-            rightTitle={this.props.user.location}
-            rightTitleStyle={{ width: 140 }}
+            title="位置"
+            rightTitle="马尼拉"
           />
         </View>
       </View>
-    )
-  }
-
-  formatGender() {
-    return this.props.user.gender === 'm' ? t('settings.male') : t('settings.female')
+    );
   }
 }
 
-const viewStyles = StyleSheet.create({
-  container: {
-    ...styles.container,
-    paddingTop: 20
-  }
-})
+export default ProfileScreen;
