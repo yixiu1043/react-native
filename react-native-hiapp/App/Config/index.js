a4e41c58bf262f3b5053375290c4f00e4ff1a05f
yueyang8389 <yueyang8389@gmail.com>
2019-07-11 18:03:10 +0800

    change appKey

--- react-native-hiapp/App/Config/index.js
+++ react-native-hiapp/App/Config/index.js
@@ -4,8 +4,10 @@ const Skin = {
 };
 
 const App = {
-  APPKEY: 'kj7swf8ok3m02',
-  imUrl: 'http://192.168.88.155:5050',
+  // APPKEY: 'kj7swf8ok3m02',
+  // imUrl: 'http://192.168.88.155:5050',
+  APPKEY: 'k51hidwqk4xab',
+  imUrl: 'https://wxchat2.mt204.com',
   domain: 'https://jsonplaceholder.typicode.com',
   // devBaseUrl: 'https://raw.githubusercontent.com/BelinChung/api-mock/master/HiApp',
   // prodBaseUrl: 'https://raw.githubusercontent.com/BelinChung/api-mock/master/HiApp',
