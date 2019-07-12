ae6549071f31929409843e78f96f724ebb356d74
yueyang8389 <yueyang8389@gmail.com>
2019-07-12 10:49:12 +0800

    modify home page

--- react-native-hiapp/App/Views/Home/index.js
+++ react-native-hiapp/App/Views/Home/index.js
@@ -3,6 +3,12 @@ import connect from 'redux-connect-decorator';
 import config from '@Config';
 import styles from '@Styles';
 import Api from '@Service/api';
+import CAT_IMG from '@assets/cat.jpg';
+import DOG_IMG from '@assets/dog.jpg';
+import PANDA_IMG from '@assets/panda.png';
+import TIBETAN_ANTELOPE_IMG from '@assets/tibetan-antelope.jpg';
+import TURTLE_IMG from '@assets/turtle.jpg';
+import WHALE_SHARK_IMG from '@assets/whale-shark.jpg';
 import {
   View,
   Text,
@@ -37,7 +43,7 @@ const viewStyles = StyleSheet.create({
 
 @connect(
   state => ({
-    chatRoomList: state.home.chatRoomList,
+    // chatRoomList: state.home.chatRoomList,
   }),
 )
 class HomeScreen extends React.Component {
@@ -45,6 +51,17 @@ class HomeScreen extends React.Component {
     ...config.defaultNavigation,
     title: '主页',
   })
+
+  state = {
+    chatRoomList: [
+      { id: 'cat', title: 'cat', url: CAT_IMG },
+      { id: 'dog', title: 'dog', url: DOG_IMG },
+      { id: 'panda', title: 'panda', url: PANDA_IMG },
+      { id: 'tibetan-antelope', title: 'tibetan-antelope', url: TIBETAN_ANTELOPE_IMG },
+      { id: 'turtle', title: 'turtle', url: TURTLE_IMG },
+      { id: 'whale-shark', title: 'whale-shark', url: WHALE_SHARK_IMG },
+    ],
+  }
 
   componentDidMount() {
     Api.fetchChatRoomList();
@@ -57,7 +74,8 @@ class HomeScreen extends React.Component {
   }
 
   render() {
-    const { chatRoomList } = this.props;
+    // const { chatRoomList } = this.props;
+    const { chatRoomList } = this.state;
     return (
       <View style={viewStyles.container}>
         <View style={viewStyles.chatRoomWrapper}>
@@ -70,9 +88,9 @@ class HomeScreen extends React.Component {
                 <TouchableHighlight
                   onPress={() => this.goChatRoom(item.id)}
                 >
-                  <Image source={{ uri: item.thumbnailUrl }} style={viewStyles.chatRoomImg} />
+                  <Image source={item.url} style={viewStyles.chatRoomImg} />
                 </TouchableHighlight>
-                <Text>{item.title.substring(0, 5)}</Text>
+                <Text>{item.title}</Text>
               </View>
             ))
           }
